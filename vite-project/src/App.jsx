3916d5fa35e34b926354725e9891b753c8808706
federicoaposada <federicoaposada@gmail.com>
2023-08-16 22:08:33 -0300

    segundo commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,51 @@
+import React, { useState } from "react";
 import Card from "./Card";
+import "./App.css";
 
 function App() {
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [showCard, setShowCard] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (input1.trim().length < 3 || input1.startsWith(" ") || input2.length < 6) {
+      setErrorMessage("Por favor chequea que la información sea correcta");
+      setShowCard(false);
+    } else {
+      setShowCard(true);
+      setErrorMessage("");
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h2>Elige un color</h2>
+      <form onSubmit={handleFormSubmit} className="form">
+        <div className="input-container">
+          <input
+            type="text"
+            value={input1}
+            onChange={(e) => setInput1(e.target.value)}
+            placeholder="Ingresá tu nombre"
+            className="input"
+          />
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            value={input2}
+            onChange={(e) => setInput2(e.target.value)}
+            placeholder="Ingresá tu color favorito (formato HEX)"
+            className="input"
+          />
+        </div>
+        <button type="submit" className="button">Enviar</button>
+        {errorMessage && <p className="error">{errorMessage}</p>}
+      </form>
+      {showCard && <Card input1={input1} input2={input2} />}
     </div>
   );
 }
--- vite-project/src/Card.jsx
+++ vite-project/src/Card.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./App.css";
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({ input1, input2 }) => {
+  return (
+    <div className="card">
+      <p>Hola {input1}!</p>
+      <p>Sabemos que tu color favorito es:</p>
+      <p>#{input2}</p>
+    </div>
+  );
 };
 
 export default Card;
